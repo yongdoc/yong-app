e3038071b1bdab1271d734f8de3644d3e9520289
yongdoc <ayahyaa17@gmail.com>
2021-12-30 16:00:09 +0700

    add link to list of pages

--- src/components/PageList.js
+++ src/components/PageList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const PageList = ({pages, title}) => {
 
   return ( 
@@ -5,8 +7,10 @@ const PageList = ({pages, title}) => {
       <h2>{title}</h2>
       {pages.map((page) => (
         <div className="page-preview" key={page.id}>
-          <h2>{page.title}</h2>
-          <p>Uploaded by {page.author}</p>
+          <Link to={`/pages/${page.id}`}>
+            <h2>{page.title}</h2>
+            <p>Uploaded by {page.author}</p>
+          </Link>
         </div>
       ))}
     </div>
