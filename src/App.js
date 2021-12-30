d6ad0a32768d7746ea06f2f0bf307cb87e87aa4c
yongdoc <ayahyaa17@gmail.com>
2021-12-30 13:44:36 +0700

    add navbar and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Create from './components/Create'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+/*
+apparently, react-router-dom v6 has some changes, that is:
+- Switch changed to Routes
+- remove 'exact' options on Route
+- etc.
+*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+    
   );
 }
 
