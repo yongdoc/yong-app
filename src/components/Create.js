4538f847dde4b893d75613d8f936e9fddf759cf0
yongdoc <ayahyaa17@gmail.com>
2022-01-11 19:51:37 +0700

    update Create.js and new data for dropdown

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,56 @@
+import useFetch from "./useFetch";
+import { useState } from "react";
+
 const Create = () => {
+  const { data: authors } = useFetch('http://localhost:8000/authors/');
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('yong');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const page = { title, body, author};
+    console.log(page);
+
+    fetch('http://localhost:8000/pages', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(page)
+    }).then(() => {
+      console.log('new page added.');
+    })
+  }
+
   return ( 
-    <div>
+    <div className="create">
       <h2>Add a New Page</h2>
+      {authors && 
+      <form onSubmit={handleSubmit}>
+        <label>Page title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Page body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Page author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          {authors.map((temp) => (
+            <option key={temp.id} value={temp.name}>{temp.name}</option>
+          ))}
+        </select>
+        <button>Add Page</button>
+      </form>
+      }
     </div>
   );
 }
